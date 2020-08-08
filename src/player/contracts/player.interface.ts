51d61abb82510337b3716a364d3a637b67aea241
Heitor Quaglia <heitor.bq@hotmail.com>
2020-08-08 09:45:39 -0300

    add player's interface

---
+++ src/player/contracts/player.interface.ts
@@ -0,0 +1,9 @@
+export interface Player {
+    readonly __id: string,
+    phoneNumber: string,
+    email: string,
+    name: string,
+    ranking: string,
+    posRanking: number,
+    urlPhoto: string
+}
--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { Player } from './contracts/player.interface';
 
 @Controller('api/v1/player')
-export class PlayerController {}
+export class PlayerController {
+
+    @Post()
+    async store(player : Player) {
+        return JSON.stringify({
+            __id: 'x1',
+            name: 'Jhon'
+        });
+    }
+}
