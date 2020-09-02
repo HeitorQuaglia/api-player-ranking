53e97ce27e3dd137aa21e7afde5d4f6e4e6bb4d8
Heitor Bernardes Quaglia <heitor.bq@hotmail.com>
2020-09-01 21:41:21 -0300

    database connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 
+require('dotenv').config()
+
 @Module({
-  imports: [PlayerModule],
+  imports: [
+
+    PlayerModule,
+
+    MongooseModule.forRoot(process.env.MONGO_CONNECTION, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
